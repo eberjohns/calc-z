a4750f43f61403b57298945b37f2ef3f40b84809
Eber Johns C D <eberjohnspro@gmail.com>
2025-06-17 21:13:03 +0530

    feat: simple ui using code

--- calc_z/lib/homepage.dart
+++ calc_z/lib/homepage.dart
@@ -8,10 +8,98 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  Widget buildButton({required String label, bool isOperator = false}) {
+    return SizedBox(
+      // width: 100,
+      // height: 100,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isOperator ? Colors.deepPurpleAccent : Colors.white,
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeRow({required List<String> buttonLabels}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: buttonLabels.map((label) {
+        return buildButton(
+          label: label,
+          isOperator:
+              label == 'C' ||
+              label == '⌫' ||
+              label == '÷' ||
+              label == '×' ||
+              label == '−' ||
+              label == '+' ||
+              label == '^' ||
+              label == '%' ||
+              label == '=',
+        );
+      }).toList(),
+    );
+  }
+
+  Widget display() {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      padding: const EdgeInsets.all(20),
+      alignment: Alignment.bottomRight,
+      child: Text(
+        '0',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget buildHomePage() {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text(
+          'Calc-Z',
+          style: TextStyle(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            display(),
+            makeRow(buttonLabels: ['C', '⌫', '%', '÷']),
+            makeRow(buttonLabels: ['7', '8', '9', '×']),
+            makeRow(buttonLabels: ['4', '5', '6', '−']),
+            makeRow(buttonLabels: ['1', '2', '3', '+']),
+            makeRow(buttonLabels: ['^', '0', '.', '=']),
+            SizedBox(height: 10), // Add some space at the bottom
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return SafeArea(child: buildHomePage());
   }
 }
--- calc_z/lib/main.dart
+++ calc_z/lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData.dark(),
+      // (
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      // ),
       home: Homepage(),
       debugShowCheckedModeBanner: false,
     );
